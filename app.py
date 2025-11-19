e0c23eab4318f405a17e224d938478d9f8525dd5
yudis2 <yudisdwi02@gmail.com>
2025-11-19 23:58:38 +0700

    test

--- app.py
+++ app.py
@@ -111,7 +111,7 @@ with tab4:
 
     @st.cache_resource
     def load_model():
-        model = joblib.load(open(Path(__file__).parent / "modelRF.joblib", "rb")) 
+        model = joblib.load(Path(__file__).parent / "modelRF.joblib")
         return model
 
     df = load_data()
